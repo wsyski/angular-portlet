6962c7202c2e3aa14b0c7ddc88211dcf7377c3ef
wsyski <wsyski@gmail.com>
2019-07-19 15:01:32 +0200

    Using osgi fragments

--- modules/angular-portlet/angular-portlet-configuration/src/main/java/com/axiell/arena/ui/modules/angular_portlet/configuration/AngularConfigurationAction.java
+++ modules/angular-portlet/angular-portlet-configuration/src/main/java/com/axiell/arena/ui/modules/angular_portlet/configuration/AngularConfigurationAction.java
@@ -4,24 +4,15 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
 import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
-import com.liferay.portal.kernel.util.StreamUtil;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import javax.portlet.PortletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component(
         immediate = true,
@@ -31,35 +22,12 @@ import java.util.stream.Collectors;
 public class AngularConfigurationAction extends DefaultConfigurationAction {
     private static final Log _log = LogFactoryUtil.getLog(
             AngularConfigurationAction.class);
-    // private URL configURL;
 
     @Override
     public String getJspPath(HttpServletRequest httpServletRequest) {
-        // return super.getJspPath(httpServletRequest);
         return "/configuration.jsp";
     }
 
-    /*
-    @Override
-    public void include(
-            PortletConfig portletConfig, HttpServletRequest httpServletRequest,
-            HttpServletResponse httpServletResponse)
-            throws Exception {
-        // super.include(portletConfig, httpServletRequest, httpServletResponse);
-        PrintWriter pw = httpServletResponse.getWriter();
-        InputStream is = configURL.openStream();
-        try {
-
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-                pw.write(br.lines().collect(Collectors.joining(System.lineSeparator())));
-            }
-        } finally {
-            is.close();
-        }
-    }
-    */
-
-    /*
     @Override
     @Reference(
             target = "(osgi.web.symbolicname=com.axiell.arena.ui.modules.angular_portlet.web)",
@@ -68,13 +36,9 @@ public class AngularConfigurationAction extends DefaultConfigurationAction {
     public void setServletContext(ServletContext servletContext) {
         super.setServletContext(servletContext);
     }
-    */
 
-    /*
     @Activate
     protected void activate(final BundleContext bundleContext, final Map<String, Object> properties) {
-        configURL = bundleContext.getBundle().getResource("/META-INF/resources/configuration.jsp");
-        _log.info("url: " + configURL);
+        _log.info(" activated: " +AngularConfigurationAction.class.getName());
     }
-     */
 }
